14dccb2260ce96888f5fd5230355e3012c9047ed
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-15 20:45:18 +0800

    feat: toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <ToastContainer />
       <Outlet />
     </div>
   );
