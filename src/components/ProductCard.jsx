f1f109b9f0a01ded0b19c4a94e8d010a65c42a96
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-23 17:30:12 +0800

    refactor: small UI adjustments

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,10 +8,10 @@ const mockData = {
 
 const ProductCard = () => {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card style={{ width: "22rem" }}>
       <Card.Img
         variant="top"
-        style={{ height: "25rem" }}
+        style={{ height: "27rem", objectFit: "cover" }}
         src="https://image-cdn.hypb.st/https%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2Fa7%2F6f%2FVest-1-1-c99de.jpg?fit=max&w=720&q=90"
       />
       <Card.Body className="text-center">
@@ -19,7 +19,9 @@ const ProductCard = () => {
           {mockData.name}
         </Card.Title>
         <Card.Text className="fs-6 ps-2 pe-2">{mockData.description}</Card.Text>
-        <Card.Text className="fs-6 fw-semibold">₱{mockData.price}</Card.Text>
+        <Card.Text className="fs-6 fw-semibold">
+          ₱{mockData.price.toLocaleString()}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
