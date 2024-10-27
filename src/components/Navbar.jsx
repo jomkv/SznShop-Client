808a8efba1f6100a58f422e096a475cf1767d81b
Rhondel Divinas Flores <rhondeldi@gmail.com>
2024-10-27 18:16:13 +0800

    refactor: update Navbar styling and add custom font
    - Update Navbar component to use react-bootstrap components
    - Add custom font "Notable" to brand text
    - Add black color to icon elements in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,51 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Col, Row } from "react-bootstrap";
+import React from "react";
+import { Navbar, Container, Col, Form, Button, Row } from "react-bootstrap";
+import "./Navbar.css";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar bg="light" expand="lg">
       <Container>
         <Row className="w-100">
           <Col lg={4} className="d-flex align-items-center fw-bold">
-            <Navbar.Brand href="#home">SZN</Navbar.Brand>
+            <Navbar.Brand href="#home" className="d-flex align-items-center">
+              <img
+                src="public/szn-logo-3.jpg"
+                alt="Store Logo"
+                width="30"
+                height="30"
+                className="d-inline-block align-top rounded-circle me-2"
+              />
+              <span className="brand-text">SZN</span>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
           </Col>
           <Col lg={8} className="d-flex justify-content-end align-items-center">
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button href="#search" variant="me-2 link">
-                <i className="bi bi-search fs-5"></i>
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="d-flex justify-content-end"
+            >
+              <Form className="d-flex me-3">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button href="#search" variant="link" className="me-2">
+                  <i className="bi bi-search fs-5 icon-black"></i>
+                </Button>
+              </Form>
+              <Button href="#profile" variant="link" className="me-2">
+                <i className="bi bi-person fs-5 icon-black"></i>
               </Button>
-            </Form>
-            <Button href="#profile" variant="me-2 link">
-              <i className="bi bi-person fs-5"></i>
-            </Button>
-            <Button href="#FAQ" variant="me-2 link">
-              <i className="bi bi-question-circle fs-5"></i>
-            </Button>
-            <Button href="#shoppingbag" variant="me-2 link">
-              <i className="bi bi-bag fs-5"></i>
-            </Button>
+              <Button href="#FAQ" variant="link" className="me-2">
+                <i className="bi bi-question-circle fs-5 icon-black"></i>
+              </Button>
+              <Button href="#shoppingbag" variant="link" className="me-2">
+                <i className="bi bi-bag fs-5 icon-black"></i>
+              </Button>
+            </Navbar.Collapse>
           </Col>
         </Row>
       </Container>
