f0930abc8045378edb06a451c64de971f75b294d
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-29 16:25:08 +0800

    feat: admin layout

--- src/AdminApp.jsx
+++ src/AdminApp.jsx
@@ -1,13 +1,50 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "./admin.css";
+import { useState } from "react";
 import AdminNavbar from "./components/AdminNavbar/AdminNavbar";
 
 function AdminApp() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div>
-      <AdminNavbar />
+    <div className="d-flex">
+      <AdminNavbar collapsed={collapsed} />
+      <div className="main">
+        <nav
+          style={{
+            padding: "1.15rem 1.5rem",
+            borderBottom: "2px dashed rgba(0, 0, 0, 0.292)",
+            minHeight: "10vh",
+            backgroundColor: "white",
+          }}
+          className="navbar-expand d-flex justify-content-between align-items-center"
+        >
+          <button
+            className="toggler-btn"
+            type="button"
+            onClick={() => {
+              setCollapsed(!collapsed);
+            }}
+          >
+            <i className="bi bi-list text-align-left" />
+          </button>
+          <img
+            src="/szn-logo-3.jpg"
+            style={{
+              width: "3rem",
+              height: "3rem",
+              borderRadius: "50%",
+            }}
+          />
+        </nav>
+        <main className="p-3 main">
+          <div className="container-fluid">
+            <Outlet />
+          </div>
+        </main>
+      </div>
       <ToastContainer />
-      <Outlet />
     </div>
   );
 }
--- src/components/AdminNavbar/AdminNavbar.jsx
+++ src/components/AdminNavbar/AdminNavbar.jsx
@@ -1,5 +1,58 @@
-function AdminNavbar() {
-  return <div>AdminNavbar</div>;
+import { Link } from "react-router-dom";
+
+function AdminNavbar({ collapsed }) {
+  return (
+    <aside
+      id="sidebar"
+      className={`sidebar-toggle ${collapsed ? "collapsed" : ""}`}
+    >
+      <p className="sidebar-logo">SZN</p>
+      <ul className="sidebar-nav p-0">
+        <li className="sidebar-item">
+          <Link to="/admin" className="sidebar-link">
+            <i className="bi bi-suitcase-lg me-3"></i>
+            <span>Dashboard</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/admin/products" className="sidebar-link">
+            <i className="bi bi-tag me-3"></i>
+            <span>Products</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/admin/customers" className="sidebar-link">
+            <i className="bi bi-people me-3"></i>
+            <span>Customers</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/admin/orders" className="sidebar-link">
+            <i className="bi bi-basket3 me-3"></i>
+            <span>Orders</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/admin/overview" className="sidebar-link">
+            <i className="bi bi-bar-chart me-3"></i>
+            <span>Overview</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/admin/settings" className="sidebar-link">
+            <i className="bi bi-gear me-3"></i>
+            <span>Settings</span>
+          </Link>
+        </li>
+      </ul>
+      <div className="sidebar-footer">
+        <Link to="/admin/#" className="sidebar-link">
+          <i className="bi bi-box-arrow-right me-3"></i>
+          <span>Logout</span>
+        </Link>
+      </div>
+    </aside>
+  );
 }
 
 export default AdminNavbar;
