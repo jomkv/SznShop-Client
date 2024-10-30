3925c2815f219cca8f0c280bc811155b12dd2b25
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-30 12:02:12 +0800

    feat: responsive navbar

--- src/components/AdminNavbar/AdminNavbar.jsx
+++ src/components/AdminNavbar/AdminNavbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function AdminNavbar({ collapsed }) {
+  const [selectedTab, setSelectedTab] = useState("dashboard");
+
   return (
     <aside
       id="sidebar"
@@ -8,47 +11,119 @@ function AdminNavbar({ collapsed }) {
     >
       <p className="sidebar-logo">SZN</p>
       <ul className="sidebar-nav p-0">
-        <li className="sidebar-item">
-          <Link to="/admin" className="sidebar-link">
-            <i className="bi bi-suitcase-lg me-3"></i>
-            <span>Dashboard</span>
+        <li
+          className={`sidebar-item p-0 ${
+            selectedTab === "dashboard" ? "active" : ""
+          }`}
+        >
+          <Link
+            to="/admin"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("dashboard");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-suitcase-lg me-3"></i>
+              <span>Dashboard</span>
+            </div>
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/admin/products" className="sidebar-link">
-            <i className="bi bi-tag me-3"></i>
-            <span>Products</span>
+        <li
+          className={`sidebar-item ${
+            selectedTab === "products" ? "active" : ""
+          }`}
+        >
+          <Link
+            to="/admin/products"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("products");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-tag me-3"></i>
+              <span>Products</span>
+            </div>
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/admin/customers" className="sidebar-link">
-            <i className="bi bi-people me-3"></i>
-            <span>Customers</span>
+        <li
+          className={`sidebar-item ${
+            selectedTab === "customers" ? "active" : ""
+          }`}
+        >
+          <Link
+            to="/admin/customers"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("customers");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-people me-3"></i>
+              <span>Customers</span>
+            </div>
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/admin/orders" className="sidebar-link">
-            <i className="bi bi-basket3 me-3"></i>
-            <span>Orders</span>
+        <li
+          className={`sidebar-item ${selectedTab === "orders" ? "active" : ""}`}
+        >
+          <Link
+            to="/admin/orders"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("orders");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-basket3 me-3"></i>
+              <span>Orders</span>
+            </div>
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/admin/overview" className="sidebar-link">
-            <i className="bi bi-bar-chart me-3"></i>
-            <span>Overview</span>
+        <li
+          className={`sidebar-item ${
+            selectedTab === "overview" ? "active" : ""
+          }`}
+        >
+          <Link
+            to="/admin/overview"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("overview");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-bar-chart me-3"></i>
+              <span>Overview</span>
+            </div>
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/admin/settings" className="sidebar-link">
-            <i className="bi bi-gear me-3"></i>
-            <span>Settings</span>
+        <li
+          className={`sidebar-item ${
+            selectedTab === "settings" ? "active" : ""
+          }`}
+        >
+          <Link
+            to="/admin/settings"
+            className="sidebar-link"
+            onClick={() => {
+              setSelectedTab("settings");
+            }}
+          >
+            <div className="link-container">
+              <i className="bi bi-gear me-3"></i>
+              <span>Settings</span>
+            </div>
           </Link>
         </li>
       </ul>
       <div className="sidebar-footer">
         <Link to="/admin/#" className="sidebar-link">
-          <i className="bi bi-box-arrow-right me-3"></i>
-          <span>Logout</span>
+          <div className="link-container">
+            <i className="bi bi-box-arrow-right me-3"></i>
+            <span>Logout</span>
+          </div>
         </Link>
       </div>
     </aside>
