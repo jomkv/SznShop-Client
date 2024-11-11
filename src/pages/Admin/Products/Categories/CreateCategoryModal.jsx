28a2cbb37311cc127ce4935cd94ded7a57e4cc37
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-11-12 00:56:44 +0800

    feat: create category with default products

--- src/pages/Admin/Products/Categories/CreateCategoryModal.jsx
+++ src/pages/Admin/Products/Categories/CreateCategoryModal.jsx
@@ -1,10 +1,13 @@
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, ToggleButton } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { toast } from "react-toastify";
 import { useCreateCategoryMutation } from "../../../../libs/rtk/api/categoryApiSlice";
 import Spinner from "../../../../components/Spinner/Spinner";
+import { useGetProductsAdminQuery } from "../../../../libs/rtk/api/productApiSlice";
+import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
 
 const schema = z.object({
   name: z
@@ -14,7 +17,16 @@ const schema = z.object({
 });
 
 function CreateCategoryModal(props) {
-  const [createCategory, { isLoading }] = useCreateCategoryMutation();
+  const [createCategory, { isSubmitting }] = useCreateCategoryMutation();
+  const { data: products, isLoading, isError } = useGetProductsAdminQuery();
+  const [productIds, setProductIds] = useState([]);
+
+  useEffect(() => {
+    if (isError) {
+      props.onHide();
+      toast.warn("An error has occured while fetching products.");
+    }
+  }, [isError]);
 
   const form = useForm({
     defaultValues: {
@@ -26,8 +38,13 @@ function CreateCategoryModal(props) {
   const { register, handleSubmit, formState, reset } = form;
   const { errors } = formState;
 
-  const handleFormSubmit = async (formData) => {
+  const handleFormSubmit = async (data) => {
     try {
+      const formData = {
+        ...data,
+        productIds,
+      };
+
       await createCategory(formData).unwrap();
       toast.success("Category created successfully");
     } catch (error) {
@@ -36,7 +53,60 @@ function CreateCategoryModal(props) {
 
     props.onHide();
     reset();
+    setProductIds([]);
   };
+
+  const handleIncludeProduct = (productId) => {
+    setProductIds((prev) => {
+      if (prev.includes(productId)) {
+        return prev.filter((id) => id !== productId); // remove id
+      } else {
+        return [...prev, productId]; // add id
+      }
+    });
+  };
+
+  const columns = [
+    {
+      name: "Image",
+      selector: (row) => row.name,
+      sortable: false,
+      cell: (row) => (
+        <img
+          src={row.images[0].url}
+          alt="Image"
+          style={{
+            width: "5rem",
+            height: "5rem",
+            objectFit: "cover",
+          }}
+        />
+      ),
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Price",
+      selector: (row) => row.price,
+      sortable: true,
+    },
+    {
+      name: "",
+      cell: (row) => (
+        <ToggleButton
+          type="radio"
+          variant="outline-dark"
+          checked={productIds.includes(row._id)}
+          onClick={() => handleIncludeProduct(row._id)}
+        >
+          Include
+        </ToggleButton>
+      ),
+    },
+  ];
 
   return (
     <Modal {...props} size="lg" centered>
@@ -57,6 +127,25 @@ function CreateCategoryModal(props) {
               {errors.name?.message}
             </Form.Control.Feedback>
           </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label className="fs-5 fw-medium">Products</Form.Label>
+
+            {isLoading && (
+              <div className="h-100 w-100 d-flex justify-content-center align-items-center">
+                <Spinner large />
+              </div>
+            )}
+            {products && (
+              <DataTable
+                columns={columns}
+                data={products.all}
+                fixedHeader={true}
+                fixedHeaderScrollHeight="55vh"
+                highlightOnHover
+                striped
+              />
+            )}
+          </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
@@ -64,9 +153,9 @@ function CreateCategoryModal(props) {
           variant="dark"
           type="submit"
           form="categoryForm"
-          disabled={isLoading}
+          disabled={isSubmitting || isLoading}
         >
-          {isLoading ? <Spinner /> : "Submit"}
+          {isSubmitting ? <Spinner /> : "Submit"}
         </Button>
       </Modal.Footer>
     </Modal>
