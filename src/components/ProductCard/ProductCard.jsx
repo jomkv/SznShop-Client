9bd800d8c0990e84666eff75674c6a77e3b353aa
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-11-21 21:53:55 +0800

    fix: product card drag at carousel

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
 
   const handleHover = () => {
     setIsHovered(true);
@@ -13,34 +15,36 @@ const ProductCard = ({ product }) => {
     setIsHovered(false);
   };
 
+  const handleRedirect = () => {
+    navigate(`/product/${product._id}`);
+  };
+
   return (
-    <Link
-      to={`/product/${product._id}`}
-      style={{
-        textDecoration: "none",
-      }}
-    >
-      <Card className="w-100">
-        <Card.Img
-          variant="top"
-          style={{ height: "24rem", objectFit: "cover" }}
-          src={isHovered ? product.images[1].url : product.images[0].url}
-          onMouseOver={handleHover}
-          onMouseLeave={handleLeaveHover}
-        />
-        <Card.Body className="text-center">
-          <Card.Title className="mt-2 mb-1 fw-bold fs-6">
-            {product.name}
-          </Card.Title>
-          <Card.Text className="fs-6 ps-2 pe-2">
-            {product.description}
-          </Card.Text>
-          <Card.Text className="fs-6 fw-semibold">
-            ₱{product.price.toLocaleString()}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Link>
+    <Card className="w-100 prod-card">
+      <Card.Img
+        variant="top"
+        className="clickable"
+        style={{ height: "24rem", objectFit: "cover" }}
+        src={isHovered ? product.images[1].url : product.images[0].url}
+        onMouseOver={handleHover}
+        onMouseLeave={handleLeaveHover}
+        onClick={handleRedirect}
+      />
+      <Card.Body className="text-center d-flex flex-column justify-content-center align-items-center">
+        <p
+          className="mt-2 mb-1 fw-bold fs-6 pointer clickable"
+          onClick={handleRedirect}
+        >
+          {product.name}
+        </p>
+        <p className="fs-6 ps-2 pe-2 clickable" onClick={handleRedirect}>
+          {product.description}
+        </p>
+        <p className="fs-6 fw-semibold clickable" onClick={handleRedirect}>
+          ₱{product.price.toLocaleString()}
+        </p>
+      </Card.Body>
+    </Card>
   );
 };
 
