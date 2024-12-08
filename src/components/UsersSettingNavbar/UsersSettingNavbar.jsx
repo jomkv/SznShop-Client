4569b7807c9c1a8221df5eb91713cf77e53858b7
Rhondel Divinas Flores <rhondeldi@gmail.com>
2024-12-09 01:14:33 +0800

    feat: refactor UsersSettingsNavbar layout to use Bootstrap grid system

--- src/components/UsersSettingNavbar/UsersSettingNavbar.jsx
+++ src/components/UsersSettingNavbar/UsersSettingNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 function UsersSettingsNavbar() {
   const [selectedTab, setSelectedTab] = useState("profile");
@@ -13,82 +13,88 @@ function UsersSettingsNavbar() {
         alignItems: "center",
       }}
     >
-      <Link
-        to="/profile"
-        className="sidebar-link"
-        onClick={() => setSelectedTab("profile")}
-        style={{ margin: "0 10px" }}
-      >
-        <Card
-          className={`mb-2 ${selectedTab === "profile" ? "active" : ""}`}
-          style={{ width: "300px", height: "150px" }}
-        >
-          <Card.Body className="d-flex align-items-center">
-            <i
-              className="bi bi-person-circle me-3"
-              style={{ fontSize: "2.5rem" }}
-            ></i>
-            <div style={{ flex: 1 }}>
-              <Card.Title className="fw-bold">PROFILE</Card.Title>
-              <Card.Text style={{ fontSize: "0.9rem" }}>
-                View and update your name, phone number, and other profile.
-                details
-              </Card.Text>
-            </div>
-          </Card.Body>
-        </Card>
-      </Link>
-
-      <Link
-        to="/address"
-        className="sidebar-link"
-        onClick={() => setSelectedTab("address")}
-        style={{ margin: "0 10px" }}
-      >
-        <Card
-          className={`mb-2 ${selectedTab === "address" ? "active" : ""}`}
-          style={{ width: "300px", height: "150px" }}
-        >
-          <Card.Body className="d-flex align-items-center mb-3">
-            <i
-              className="bi bi-house me-3 mt-3"
-              style={{ fontSize: "2.5rem" }}
-            ></i>
-            <div style={{ flex: 1 }}>
-              <Card.Title className="fw-bold">ADDRESS</Card.Title>
-              <Card.Text style={{ fontSize: "0.9rem" }}>
-                Save, edit, remove or set default address on your account.
-              </Card.Text>
-            </div>
-          </Card.Body>
-        </Card>
-      </Link>
-
-      <Link
-        to="/usersorders"
-        className="sidebar-link"
-        onClick={() => setSelectedTab("orders")}
-        style={{ margin: "0 10px" }}
-      >
-        <Card
-          className={`mb-2 ${selectedTab === "orders" ? "active" : ""}`}
-          style={{ width: "300px", height: "150px" }}
-        >
-          <Card.Body className="d-flex align-items-center">
-            <i
-              className="bi bi-basket3 me-3"
-              style={{ fontSize: "2.5rem" }}
-            ></i>
-            <div style={{ flex: 1 }}>
-              <Card.Title className="fw-bold">ORDERS</Card.Title>
-              <Card.Text style={{ fontSize: "0.9rem" }}>
-                View current and past orders, track your current order or file
-                for a return.
-              </Card.Text>
-            </div>
-          </Card.Body>
-        </Card>
-      </Link>
+      <Row>
+        <Col>
+          <Link
+            to="/profile"
+            className="sidebar-link"
+            onClick={() => setSelectedTab("profile")}
+            style={{ margin: "0 10px" }}
+          >
+            <Card
+              className={`mb-2 ${selectedTab === "profile" ? "active" : ""}`}
+              style={{ width: "100%", maxwidth: "300px", height: "150px" }}
+            >
+              <Card.Body className="d-flex align-items-center">
+                <i
+                  className="bi bi-person-circle me-3"
+                  style={{ fontSize: "2.5rem" }}
+                ></i>
+                <div style={{ flex: 1 }}>
+                  <Card.Title className="fw-bold">PROFILE</Card.Title>
+                  <Card.Text style={{ fontSize: "0.9rem" }}>
+                    View and update your name, phone number, and other profile.
+                    details
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+        <Col>
+          <Link
+            to="/address"
+            className="sidebar-link"
+            onClick={() => setSelectedTab("address")}
+            style={{ margin: "0 10px" }}
+          >
+            <Card
+              className={`mb-2 ${selectedTab === "address" ? "active" : ""}`}
+              style={{ width: "100%", maxwidth: "300px", height: "150px" }}
+            >
+              <Card.Body className="d-flex align-items-center mb-3">
+                <i
+                  className="bi bi-house me-3 mt-3"
+                  style={{ fontSize: "2.5rem" }}
+                ></i>
+                <div style={{ flex: 1 }}>
+                  <Card.Title className="fw-bold">ADDRESS</Card.Title>
+                  <Card.Text style={{ fontSize: "0.9rem" }}>
+                    Save, edit, remove or set default address on your account.
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+        <Col>
+          <Link
+            to="/usersorders"
+            className="sidebar-link"
+            onClick={() => setSelectedTab("orders")}
+            style={{ margin: "0 10px" }}
+          >
+            <Card
+              className={`mb-2 ${selectedTab === "orders" ? "active" : ""}`}
+              style={{ width: "100%", maxwidth: "300px", height: "150px" }}
+            >
+              <Card.Body className="d-flex align-items-center">
+                <i
+                  className="bi bi-basket3 me-3"
+                  style={{ fontSize: "2.5rem" }}
+                ></i>
+                <div style={{ flex: 1 }}>
+                  <Card.Title className="fw-bold">ORDERS</Card.Title>
+                  <Card.Text style={{ fontSize: "0.9rem" }}>
+                    View current and past orders, track your current order or
+                    file for a return.
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/pages/User/Settings/Settings.jsx
+++ src/pages/User/Settings/Settings.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import UsersSettingsNavbar from "../../../components/UsersSettingNavbar/UsersSettingNavbar";
+import { Col, Container } from "react-bootstrap";
 
 function Settings() {
   return (
-    <div
+    <Container
       style={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        height: "60vh",
       }}
     >
       <div
         style={{ marginTop: "20px", fontSize: "30px", fontWeight: "bold" }}
-        className="fs-1"
+        className="fs-1 mb-5"
       >
         ACCOUNT
       </div>
       <div style={{ display: "flex", alignItems: "center", flex: 1 }}>
         <UsersSettingsNavbar />
       </div>
-    </div>
+    </Container>
   );
 }
 
