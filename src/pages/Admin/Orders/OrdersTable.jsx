2d806ec51c8f5f71a3e6005104314f24117e9190
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-01-26 11:12:16 +0800

    feat: admin orders table updates

--- src/pages/Admin/Orders/OrdersTable.jsx
+++ src/pages/Admin/Orders/OrdersTable.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Spinner } from "react-bootstrap";
+import { Col, Form, Spinner } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import { formatDate, getOrderTotal } from "../../../utils/helper";
 import ActionButtons from "./ActionButtons";
@@ -8,7 +8,28 @@ function OrdersTable({ orders, isLoading, isSuccess }) {
   const [ordersResult, setOrdersResult] = useState(orders);
   const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    const result = orders.filter((order) => {
+      const searchLower = search.toLowerCase();
+      return (
+        order._id.toLowerCase().includes(searchLower) ||
+        `${order.address.firstName} ${order.address.lastName}`
+          .toLowerCase()
+          .includes(searchLower) ||
+        formatDate(order.createdAt).toLowerCase().includes(searchLower) ||
+        order.status.toLowerCase().includes(searchLower) ||
+        getOrderTotal(order).toString().toLowerCase().includes(searchLower)
+      );
+    });
+    setOrdersResult(result);
+  }, [search, orders]);
+
   const columns = [
+    {
+      name: "Order ID",
+      selector: (row) => row._id,
+      sortable: true,
+    },
     {
       name: "Customer Name",
       selector: (row) => `${row.address.firstName} ${row.address.lastName}`,
@@ -35,41 +56,48 @@ function OrdersTable({ orders, isLoading, isSuccess }) {
     },
   ];
 
-  useEffect(() => {
-    if (!isSuccess) return;
-
-    // const result = orders.filter((order) => {
-    //   return order.
-    // })
-  }, [search]);
-
   return (
     <>
       {isLoading && <Spinner />}
       {isSuccess && orders && (
-        <DataTable
-          columns={columns}
-          data={ordersResult}
-          fixedHeaderScrollHeight="500px"
-          customStyles={{
-            table: {
-              style: {
-                width: "100%", // Adjust the width as needed
+        <>
+          <div className="d-flex w-100 align-items-center mt-2 mb-2">
+            <p className="fs-5 mb-0 me-1">Search:</p>
+            <Form.Control
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{
+                width: "15rem",
+              }}
+            />
+          </div>
+
+          <Col sm={6}></Col>
+          <DataTable
+            columns={columns}
+            data={ordersResult}
+            fixedHeaderScrollHeight="500px"
+            customStyles={{
+              table: {
+                style: {
+                  width: "100%", // Adjust the width as needed
+                },
               },
-            },
-            headCells: {
-              style: {
-                fontWeight: "bold",
-                fontSize: "15px",
+              headCells: {
+                style: {
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                },
               },
-            },
-            cells: {
-              style: {
-                fontSize: "18px", // Adjust the font size as needed
+              cells: {
+                style: {
+                  fontSize: "18px", // Adjust the font size as needed
+                },
               },
-            },
-          }}
-        />
+            }}
+          />
+        </>
       )}
     </>
   );
